5d7dece74d948be879d47219a4376445de0b04ba
Julie Lacresse <61269042+leiju91@users.noreply.github.com>
2021-02-28 20:22:30 +0100

    js filtre food category

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.onscroll = () => {
     }
 }
 
-//TOP NAV
+// Top nav
 
 let menuItems = document.getElementsByClassName('menu-item');
 Array.from(menuItems).forEach((items, index) => {
@@ -17,5 +17,20 @@ Array.from(menuItems).forEach((items, index) => {
         let currMenu = document.querySelector('.menu-item.active');
         currMenu.classList.remove('active');
         items.classList.add('active');
+    }
+})
+
+// food category
+
+let foodMenuList = document.querySelector('.food-item-wrap');
+let foodCategory = document.querySelector('.food-category');
+let categories = foodCategory.querySelectorAll('button');
+
+Array.from(categories).forEach((item, index) => {
+    item.onclick = (e) => {
+        let currCat = foodCategory.querySelector('button.active');
+        currCat.classList.remove('active');
+        e.target.classList.add('active');
+        foodMenuList.classList = 'food-item-wrap ' + e.target.getAttribute('data-food-type');
     }
 })
