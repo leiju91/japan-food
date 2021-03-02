22278d3fafb8896dbc80305585223ecb2329d13e
Julie Lacresse <61269042+leiju91@users.noreply.github.com>
2021-03-02 20:43:56 +0100

    responsive

--- index.js
+++ index.js
@@ -64,3 +64,16 @@ loop = () => {
 }
 
 loop();
+
+// mobile nav
+
+let bottomNavItems = document.querySelector('.mb-nav-item');
+let bottomMove = document.querySelector('.mb-move-item');
+bottomNavItems.forEach((item, index) => {
+    item.onclick = (e) => {
+        let currItem = document.querySelector('.mb-nav-item.active');
+        currItem.classList.remove('active');
+        item.classList.add('active');
+        bottomMove.style.left = index * 25 + '%';
+    }
+})
